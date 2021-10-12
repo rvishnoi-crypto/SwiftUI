662435cf77a1aaa63fc0d9bf79153d31317d28f1
hj <rvishnoi@itsupports-MacBook-Pro.local>
2021-10-12 12:30:26 -0400

    first add

---
+++ newProject/newProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  newProjectApp.swift
+//  newProject
+//
+//  Created by Raghav Vishnoi on 2021-10-12.
+//
+
+import SwiftUI
+
+@main
+struct newProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
